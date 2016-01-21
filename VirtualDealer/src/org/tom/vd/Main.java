da045e4ddeff06e310a9873d28ee6e4e6ddc117a
tangrufeng <ex-tangrufeng001@pingan.com.cn>
2016-01-21 17:22:32 +0800

    修改log4j初始化路径

--- VirtualDealer/src/org/tom/vd/Main.java
+++ VirtualDealer/src/org/tom/vd/Main.java
@@ -1,7 +1,11 @@
 package org.tom.vd;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Properties;
 
 import org.apache.log4j.PropertyConfigurator;
@@ -11,40 +15,65 @@ import org.tom.vd.mina.MinaGameInterface;
 import com.effecia.mina.MinaCommandPo;
 
 /**
- * <p>Title: VirtualDealer.java</p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2014</p>
- * <p>Company: jc-yt.com</p>
+ * <p>
+ * Title: VirtualDealer.java
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2014
+ * </p>
+ * <p>
+ * Company: jc-yt.com
+ * </p>
+ * 
  * @author tang
  * @date 2016-1-10 下午3:42:44
  * @version 1.0
- *
+ * 
  */
 public class Main {
-	private void initLog4j(){
+	private void initLog4j() {
 		System.out.println("初始化log4j........");
 		Properties prop = new Properties();
-		InputStream in = Main.class.getResourceAsStream("/log4j.properties");
+		InputStream in = null;
 		try {
+			String path = URLDecoder.decode(this.getClass()
+					.getProtectionDomain().getCodeSource().getLocation()
+					.getPath(), "UTF-8");
+			File file = new File(path);
+			String log4jPorp = file.getParentFile().getAbsolutePath()
+					+ File.separator + "log4j.properties";
+			System.out.println(log4jPorp);
+			in = new FileInputStream(new File(log4jPorp));
 			prop.load(in);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+				}
+			}
 		}
 		PropertyConfigurator.configure(prop);
+		System.out.println("初始化log4j结束........");
 	}
-	
-	private void initMina(){
+
+	private void initMina() {
 		System.out.println("初始话Mina，并注册..........");
 		new MinaGameInterface();
-		MinaCommandPo regiestPo  = new MinaCommandPo();
+		MinaCommandPo regiestPo = new MinaCommandPo();
 		regiestPo.setModuleType("PLATFORM");
 		regiestPo.setRequestType("REQUEST");
 		regiestPo.setCommandType(ApCommand.TABLE_INIT.toString());
 		MinaGameInterface.ioSession.write(regiestPo);
 		System.out.println("初始话Mina，并注册结束..........");
 	}
-	
-	private void init(){
+
+	private void init() {
 		initLog4j();
 	}
 
@@ -52,7 +81,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		System.out.println("every thing is begin.....");
+		new Main().init();
+		new VirtualDealer().start();
 	}
 
 }
